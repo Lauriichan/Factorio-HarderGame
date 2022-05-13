e3c1e91c726b2c31188f31d1c7689a4bfdb68c91
Lauriichan <laura.endress@syntaxphoenix.com>
2022-05-13 10:46:31 +0200

    Move build.sh and add changes.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 mkdir tmp
 rsync -av --progress * /tmp --exclude tmp
-rm -f tmp/build.sh
+rm -f -r tmp/scripts/
 rm -f -r tmp/.github/
 rm -f tmp/.gitignore
 move tmp/ $1/
 rm -f -r tmp/
-zip $1-$2.zip -r $1/
+zip $1_$2.zip -r $1/
 rm -f -r $1/
---
+++ scripts/changes.sh
@@ -0,0 +1,11 @@
+VAR=
+while read -r line; do {
+    if [[ "$line" == \-+ ]]; then
+        if [ $VAR == "" ]; then 
+            continue;
+        fi
+        echo $VAR
+        return
+    fi
+    VAR=$VAR + '\n' + $line
+}; done < changelog.txt
