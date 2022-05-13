a3b3ef4b7a116e451f4c99690e880370b6d2ef08
Lauriichan <laura.endress@syntaxphoenix.com>
2022-05-13 11:02:42 +0200

    Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 mkdir tmp
-rsync -av --progress * /tmp --exclude tmp
+rsync -av --progress * tmp --exclude tmp
 rm -r tmp/scripts
 rm -r tmp/.github
 rm -f tmp/.gitignore
-mv tmp/ $1/
-zip $1_$2.zip -r $1
+mv tmp $1
+zip -r $1_$2.zip $1
 echo "$1_$2.zip"
 rm -r $1
