9dc5402b4897f6c4b51f30dd2237f3ff093e44f2
Laura Endreß <laura.endress@syntaxphoenix.com>
2022-05-13 10:55:49 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,7 @@ rsync -av --progress * /tmp --exclude tmp
 rm -r tmp/scripts
 rm -r tmp/.github
 rm -f tmp/.gitignore
-move tmp/ $1/
-rm -r tmp
+mv tmp/ $1/
 zip $1_$2.zip -r $1/
 echo "$1_$2.zip"
 rm -r $1
