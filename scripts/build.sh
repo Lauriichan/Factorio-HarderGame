8297482906c63f1f8fa6eb624899642af1034901
Lauriichan <laura.endress@syntaxphoenix.com>
2022-05-13 10:52:47 +0200

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ rsync -av --progress * /tmp --exclude tmp
 rm -f -r tmp/scripts/
 rm -f -r tmp/.github/
 rm -f tmp/.gitignore
-move tmp/ $1/
+mv tmp/ $1/
 rm -f -r tmp/
 zip $1_$2.zip -r $1/
 echo "$1_$2.zip"
